3822df289a16dc59dcbb126239f421a0c108c7b0
dionisioR <72356766+dionisioR@users.noreply.github.com>
2024-07-28 21:09:43 -0300

    Update 2024/07/29 21:09
    Update 2024/07/29 21:09

--- 009_trabalhando_com_funcoes/index.php
+++ 009_trabalhando_com_funcoes/index.php
@@ -9,13 +9,84 @@
 </head>
 <body>
     <header class="text-center p-5">
-        <h1 class="display-3">x</h1>
+        <h1 class="display-3">Trabalhando com Funções</h1>
     </header>
     
     <main class="container">
-    <h2 class="display-6">**--**</h2>
+    <h2 class="display-6">Função Declarada</h2>
         <?php
-           
+           require __DIR__."/funcoes.php";
+              $result = functionName("Jesus", "Maria", "José");
+              echo '<pre>';
+              var_dump($result);
+              echo '</pre>';
+        ?>
+        <hr>
+
+        <?php
+            $result = argumentosOpcionais("Anna", "Maria");
+            echo '<pre>';
+            var_dump($result);
+            echo '</pre>';
+            echo '<hr/>';
+
+            $result = argumentosOpcionais("Anna", "Maria", false);
+            echo '<pre>';
+            var_dump($result);
+            echo '</pre>';
+            echo '<hr/>';
+
+            $result = argumentosOpcionais("Anna", "Maria", false, 22);
+            echo '<pre>';
+            var_dump($result);
+            echo '</pre>';
+            echo '<hr/>';
+
+        ?>
+        <br><br>
+
+    <h2 class="display-6">Acesso Global</h2>
+        <?php
+           $peso = 86;
+           $altura = 1.80;
+           echo IMC();
+        ?>
+        <br><br>
+    <h2 class="display-6">Argumento estático</h2>
+        <?php
+            // Como o argumento é estático os valores são somandos automaticamente
+            // toda vez que a função for chamada.
+           $valor = totalPagar(100);  
+           echo $valor;  // 100
+           $valor = totalPagar(150);
+           echo $valor;  // 250
+           $valor = totalPagar(55);
+           echo $valor;  // 305
+        ?>
+        <br><br>
+    <h2 class="display-6">Argumentos Dinâmicos</h2>
+        <?php
+           // utilizado quando não sabemos a quantidade de argumentos ou quais argumentos serão passados
+
+           echo '<pre>';
+           var_dump(argumentosDinamicos());
+           echo '</pre>';
+           echo '<hr>';
+
+           echo '<pre>';
+           var_dump(argumentosDinamicos('Anna'));
+           echo '</pre>';
+           echo '<hr>';
+
+           echo '<pre>';
+           var_dump(argumentosDinamicos('Anna', 'Maria'));
+           echo '</pre>';
+           echo '<hr>';
+
+           echo '<pre>';
+           var_dump(argumentosDinamicos('Anna', 'Maria', 'João'));
+           echo '</pre>';
+           echo '<hr>';
         ?>
 
     </main>
